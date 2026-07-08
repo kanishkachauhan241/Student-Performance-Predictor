7b3b5ee686f07e42f0eada453444b2f63a5a7adf
kanishkachauhan241 <kanishkachauhan006@gmail.com>
2026-07-08 23:32:26 +0530

    Day 16: Connect Flask app with ML model

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
+# Load the trained model
+model = joblib.load("student_model.pkl")
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    prediction = None
+    performance = None
+
+    if request.method == "POST":
+
+        study_hours = float(request.form["study_hours"])
+        attendance = float(request.form["attendance"])
+        assignments = float(request.form["assignments"])
+
+        student = pd.DataFrame({
+            "StudyHours": [study_hours],
+            "Attendance": [attendance],
+            "AssignmentsCompleted": [assignments]
+        })
+
+        marks = model.predict(student)[0]
+
+        prediction = round(marks, 2)
+
+        if marks >= 90:
+            performance = "Excellent"
+        elif marks >= 75:
+            performance = "Very Good"
+        elif marks >= 60:
+            performance = "Good"
+        elif marks >= 40:
+            performance = "Average"
+        else:
+            performance = "Needs Improvement"
+
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        performance=performance
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
